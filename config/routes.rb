b31e97cd3df949d18509c77737610976a62d7178
placidowang <placidow@gmail.com>
2020-02-25 09:57:20 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  resources :accounts
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
+  get 'admi/dashboard'
+
+  get 'admi/stats'
+
+  get 'admi/financials'
+
+  get 'admi/settings'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'categories/show'
 
